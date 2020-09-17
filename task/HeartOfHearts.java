1df91df13ed23f241c2ae683ba21141babb00ae8
paulaSiemens <pasi@itu.dk>
2020-09-17 14:51:22 +0200

    Main

---
+++ task/HeartOfHearts.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class HeartOfHearts {
+    public static void main(String[] args) {
+
+
+        var in = new Scanner(System.in); //Scanner input = new Scanner(System.in);
+        // you make a new object of the Scanner class
+        // (so you make a new "Scanner") and you store it
+        // in the variable input .
+
+        var N = in.nextInt();
+        System.out.println("Just like this:");
+        System.out.println("♡♡♥♥♥♡♡♡♥♥♥♡♡");
+        System.out.println("♡♥♥♥♥♥♡♥♥♥♥♥♡");
+        System.out.println("♥♥♥♥♥♥♥♥♥♥♥♥♥");
+        System.out.println("♡♥♥♥♥♥♥♥♥♥♥♥♡");
+        System.out.println("♡♡♥♥♥♥♥♥♥♥♥♡♡");
+        System.out.println("♡♡♡♥♥♥♥♥♥♥♡♡♡");
+        System.out.println("♡♡♡♡♥♥♥♥♥♡♡♡♡");
+        System.out.println("♡♡♡♡♡♥♥♥♡♡♡♡♡");
+        System.out.println("♡♡♡♡♡♡♥♡♡♡♡♡♡");
+        System.out.println("but in size "+N);
+
+
+    }
+}
